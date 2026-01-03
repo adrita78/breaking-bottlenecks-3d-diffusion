be3f9f3cacea7e4d23d21b6a0dabae4c9f3215db
adritad <98999592+adrita78@users.noreply.github.com>
2026-01-04 01:00:04 +0530

    Add files via upload

---
+++ scripts/training.py
@@ -0,0 +1,91 @@
+import argparse
+
+from utils import dist_util, logger
+from utils.featurization import construct_loader
+from utils.resample import create_named_schedule_sampler
+from utils.script_util import (
+    model_and_diffusion_defaults,
+    data_defaults,
+    create_model_and_diffusion,
+    args_to_dict,
+    add_dict_to_argparser,
+)
+from utils.training_util import TrainLoop
+
+def main():
+    args = create_argparser().parse_args()
+
+    dist_util.setup_dist()
+    logger.configure()
+
+    logger.log("creating model and diffusion...")
+    model, diffusion = create_model_and_diffusion(
+        **args_to_dict(args, model_and_diffusion_defaults().keys())
+    )
+    model.to(dist_util.dev())
+
+    schedule_sampler = create_named_schedule_sampler(
+        args.schedule_sampler, diffusion
+    )
+
+    logger.log("creating data loader...")
+    train_loader, val_loader = construct_loader(
+        data_dir=args.data_dir,
+        split_path=args.split_path,
+        cache=args.cache,
+        dataset=args.dataset,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        limit_train_mols=args.limit_train_mols,
+        modes=("train", "val"),
+    )
+
+    logger.log("training...")
+    TrainLoop(
+        model=model,
+        diffusion=diffusion,
+        data=train_loader,
+        batch_size=args.batch_size,
+        microbatch=args.microbatch,
+        lr=args.lr,
+        epochs=args.epochs,
+        ema_rate=args.ema_rate,
+        log_interval=args.log_interval,
+        save_interval=args.save_interval,
+        resume_checkpoint=args.resume_checkpoint,
+        use_fp16=args.use_fp16,
+        fp16_scale_growth=args.fp16_scale_growth,
+        schedule_sampler=schedule_sampler,
+        weight_decay=args.weight_decay,
+    ).run_loop()
+
+
+
+def create_argparser():
+    defaults = dict(
+        # training
+        schedule_sampler="uniform",
+        lr=1e-4,
+        weight_decay=0.0,
+        epochs=1000,
+        batch_size=32,
+        microbatch=-1,
+        ema_rate="0.9999",
+        log_interval=10,
+        save_interval=2,
+        resume_checkpoint="",
+        use_fp16=False,
+        fp16_scale_growth=1e-3,
+    )
+
+    defaults.update(data_defaults())
+    defaults.update(model_and_diffusion_defaults())
+
+    parser = argparse.ArgumentParser()
+    add_dict_to_argparser(parser, defaults)
+    return parser
+
+
+if __name__ == "__main__":
+    main()
+
