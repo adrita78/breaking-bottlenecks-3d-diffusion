4b05de12ac7cd156ca922f2d2d24986c0b742d6f
adritad <98999592+adrita78@users.noreply.github.com>
2026-01-11 04:55:36 +0530

    Add files via upload

---
+++ run_alone.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MASTER=$1
+
+MODEL_FLAGS="--image_size 32 --num_channels 128 --num_res_blocks 3 --dropout 0.3"
+DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule linear"
+TRAIN_FLAGS="--lr 1e-4 --batch_size 128"
+
+
+export DDDM_LOGDIR=./checkpoints
+
+torchrun \
+--standalone \
+--nproc_per_node=2 \
+./scripts/image_train.py --data_dir datasets/cifar_train $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
+
+
