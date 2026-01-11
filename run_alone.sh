2db7a35b7f625d93be78d37573680abdf0e3a72c
adritad <98999592+adrita78@users.noreply.github.com>
2026-01-11 10:25:01 +0530

    Update run_alone.sh

--- run_alone.sh
+++ run_alone.sh
@@ -2,7 +2,27 @@
 
 MASTER=$1
 
-MODEL_FLAGS="--image_size 32 --num_channels 128 --num_res_blocks 3 --dropout 0.3"
+MODEL_FLAGS="\
+--model_type jamba \
+--channels 512 \
+--depth 6 \
+--heads 4 \
+--headdim 16 \
+--num_layers 8 \
+--attn_dropout 0.25 \
+--in_node_nf 512 \
+--out_node_nf 512 \
+--in_edge_nf 512 \
+--hidden_nf 512 \
+--pe_dim 20 \
+--context_dim 16 \
+--time_embed_dim 16 \
+--d_state 512 \
+--d_conv 256 \
+--order_by_degree True \
+--shuffle_ind 0 \
+--num_tokens 2000 \
+--num_graphs 55000"
 DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule cosine --beta_start 1e-5 --beta_end 2e-2 --loss_type "ph" --c 0.000069"
 TRAIN_FLAGS="--lr 1e-4 --batch_size 128"
 
