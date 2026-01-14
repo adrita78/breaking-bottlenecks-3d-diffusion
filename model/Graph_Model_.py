c495fa4071ad9074eb89a3a8c309ae7c2a835fea
adritad <98999592+adrita78@users.noreply.github.com>
2026-01-14 11:16:46 +0530

    Update Graph_Model_.py

--- model/Graph_Model_.py
+++ model/Graph_Model_.py
@@ -3,7 +3,7 @@ import argparse
 import os.path as osp
 from typing import Any, Dict, Optional
 import math
-import egnn_clean as eg
+import model.egnn_clean as eg
 from torch.nn import (
     BatchNorm1d,
     Embedding,
@@ -208,5 +208,6 @@ def timestep_embedding(timesteps, dim, max_period=10000):
       if dim % 2:
         embedding = th.cat([embedding, th.zeros_like(embedding[:, :1])], dim=-1)
       return embedding    
+
 
 
