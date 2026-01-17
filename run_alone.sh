fd5cf43ef0888eec3ce96ccb70c05a2fc6d68dad
adritad <98999592+adrita78@users.noreply.github.com>
2026-01-17 11:04:51 +0530

    Update run_alone.sh

--- run_alone.sh
+++ run_alone.sh
@@ -18,6 +18,10 @@ TRAIN_FLAGS="--schedule_sampler uniform --lr 3e-4 --min_lr 3e-5 \
 
 
 export DDDM_LOGDIR=./checkpoints
-$MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
 
+
+torchrun \
+--standalone \
+--nproc_per_node=8 \
+./scripts/train.py --data_dir datasets/cifar_train $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
 
