a139339bdb30f3670cdc953a63b3d4595a2c1fe1
adritad <98999592+adrita78@users.noreply.github.com>
2026-01-21 07:33:14 +0530

    Update fp16_util.py

--- utils/fp16_util.py
+++ utils/fp16_util.py
@@ -73,4 +73,5 @@ def zero_grad(model_params):
         # Taken from https://pytorch.org/docs/stable/_modules/torch/optim/optimizer.html#Optimizer.add_param_group
         if param.grad is not None:
             param.grad.detach_()
+
             param.grad.zero_()
