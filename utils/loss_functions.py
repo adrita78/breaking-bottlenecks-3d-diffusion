ba531728bd0cba647986d25df3378ec076ca0dd4
adritad <98999592+adrita78@users.noreply.github.com>
2026-01-24 21:18:47 +0530

    Update loss_functions.py

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -9,10 +9,19 @@ def ph_loss(input,target,c):
     return th.sqrt((input - target) ** 2 + c**2) - c
 
 
-def mean_flat(tensor, batch):
+#def mean_flat(tensor, batch):
     sum_per_graph = scatter(tensor, batch, dim=0, reduce="sum")
     count_per_graph = scatter(
         torch.ones(batch.size(0), 1, dtype=tensor.dtype, device=tensor.device),
         batch, dim=0, reduce="sum"
     )
-    return sum_per_graph / count_per_graph
+    #return sum_per_graph / count_per_graph
+
+def mse_graph(input, target):
+    """
+    input, target: [B, d] graph-level embeddings
+    returns: scalar graph-level MSE
+    """
+    assert input.shape == target.shape, "Input and target must have the same shape"
+    return ((input - target) ** 2).sum(dim=1).mean()
+
