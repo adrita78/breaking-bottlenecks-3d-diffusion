fd1225f8f511f14d8eb36504d6961ed9f568caea
adritad <98999592+adrita78@users.noreply.github.com>
2026-02-04 19:42:02 +0530

    Update generate_confs_corrected.py

--- generate_confs_corrected.py
+++ generate_confs_corrected.py
@@ -1,1 +1,29 @@
+import random
+import os
+import pandas as pd
+from featurize_mol import featurize_mol, featurize_mol_from_smiles
+import torch, copy
+from copy import deepcopy
+import numpy as np
+from torch_geometric.data import Dataset
+from torch_geometric.loader import DataLoader
+from rdkit import Chem, Geometry
+from rdkit.Chem import AllChem
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Geometry import Point3D
 
+#from utils.utils import time_limit, TimeoutException
+from visualise import PDBFile
+from spyrmsd import molecule, graph
+from rdkit.Geometry import Point3D
+from copy import deepcopy
+from argparse import ArgumentParser
+import pickle
+import pickle
+from tqdm import tqdm
+import random
+import torch
+import yaml
+from model_dir.LapPE import AddCustomLaplacianEigenPE
+from model_dir.Graph_Model_ import GraphModel
